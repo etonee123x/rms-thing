c150eede75b369e9c7650ab21851c4c8c9d81a93
Akex Jester <alex@blackseadigital.com>
2022-01-22 21:45:04 +0200

    something

---
+++ src/components/FileExplorer.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="file-explorer">
+        <li v-for="file in files" :key="file.name" v-text="file.name"></li>
+        <li>puls</li>
+    </ul>
+</template>
+<script>
+    export default {
+        name: 'FileExplorer',
+        props: ['files']
+    }
+</script>
+<style></style>
