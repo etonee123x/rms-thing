6feda7f61f25a732cfca07fdce06d7eb2e5a2779
E123WH <e123wh@mail.ru>
2022-02-11 00:45:06 +0300

    removed useless logs

--- src/views/RMSTestingPage/RMSTestingPage.vue
+++ src/views/RMSTestingPage/RMSTestingPage.vue
@@ -6,7 +6,12 @@
       <div class="page__results">
         <pre>{{ actionsInProcess }}</pre>
         <BaseWaiter :is-waiting="isWaiting" :actions-in-process="actionsInProcess">
-          <GraphSpectrum class="page__graph" v-if="spectrumValues" :spectrum-values="spectrumValues" :nyquist-frequency="nyquistFrequency" />
+          <GraphSpectrum
+            v-if="spectrumValues"
+            class="page__graph"
+            :spectrum-values="spectrumValues"
+            :nyquist-frequency="nyquistFrequency"
+          />
           <GraphRMSValues class="page__graph" :rms-values="rmsValues" />
         </BaseWaiter>
       </div>
@@ -38,39 +43,29 @@ const uploaded = async (filesArray: File[]) => {
   let wavData;
   if (filesArray[0].type === 'audio/wav') wavData = Buffer.from(await filesArray[0].arrayBuffer());
   else {
-    console.time('converting');
     waiterStore.addAction(actionsList.CONVERTING);
     wavData = await convertSingleFile(filesArray[0]);
     waiterStore.removeAction(actionsList.CONVERTING);
-    console.timeEnd('converting');
   }
 
-  console.time('the loudest segment');
   waiterStore.addAction(actionsList.GETTING_THE_LOUDEST_SEGMENT);
   const theAnalyzer = new TheAnalyzer(wavData);
   waiterStore.removeAction(actionsList.GETTING_THE_LOUDEST_SEGMENT);
-  console.timeEnd('the loudest segment');
 
   nyquistFrequency.value = theAnalyzer.sampleRate / 2;
 
-  console.time('rms');
   waiterStore.addAction(actionsList.GETTING_RMS);
   const rmsOptions: RMSOptions = theAnalyzer.DEFAULT_RMS_OPTIONS_FOR_THIS_SAMPLE_RATE;
   theAnalyzer.getRMS(rmsOptions, false).then(result => {
     rmsValues.value = result;
     waiterStore.removeAction(actionsList.GETTING_RMS);
-    console.timeEnd('rms');
-    console.log(rmsValues.value);
   });
 
-  console.time('spectrum');
   waiterStore.addAction(actionsList.GETTING_SPECTRUM);
   const spectrumOptions: SpectrumOptions = TheAnalyzer.DEFAULT_SPECTRUM_OPTIONS;
   theAnalyzer.getSpectrum(spectrumOptions).then(result => {
     spectrumValues.value = result;
     waiterStore.removeAction(actionsList.GETTING_SPECTRUM);
-    console.timeEnd('spectrum');
-    console.log(spectrumValues.value);
   });
 };
 </script>
